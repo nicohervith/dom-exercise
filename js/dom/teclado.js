e35e60bba0efcecf13cde1dbf1f23e7194d794ca
nicohervith <niko_hervith@hotmail.com>
2021-10-18 12:14:13 -0300

    section 3

--- js/dom/teclado.js
+++ js/dom/teclado.js
@@ -1,4 +1,56 @@
 const d = document;
+let x= 0,
+y=0; 
+
+export function moveBall (e,ball,stage){
+  const $ball=d.querySelector(ball),
+  $stage = d.querySelector(stage),
+  limitsBall = $ball.getBoundingClientRect(),
+  limitsStage= $stage.getBoundingClientRect()
+
+ /* console.log(limitsBall,limitsStage)
+  console.log(e.keyCode);
+  console.log(e.key); */
+
+  switch (e.keyCode) {
+    case 37:
+      //move("left") 
+      if(limitsBall.left > limitsStage.left) {
+         e.preventDefault();
+        x--;
+      }
+      break;
+
+    case 38:
+      //move("up");
+      if (limitsBall.top > limitsStage.top) {
+       e.preventDefault();
+      y--;
+      }
+      break;
+
+    case 39:
+      //move("right");
+      if (limitsBall.right < limitsStage.right) {
+       e.preventDefault();
+      x++;
+      }
+      break;
+
+    case 40:
+      //move("down");
+      if (limitsBall.bottom< limitsStage.bottom) {
+       e.preventDefault();
+      y++;
+      }
+      break;
+
+    default:
+      break;
+  }
+  $ball.style.transform = `translate(  ${x * 10}px, ${y * 10}px)`;
+
+}
 
 export function shorcuts(e ) {
   
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,8 @@
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
 import { shorcuts } from "./dom/teclado.js";
+import { moveBall } from "./dom/teclado.js";
+import countdown from "./dom/cuenta_regresiva.js";
 
 const d=document;
 
@@ -8,8 +10,10 @@ d.addEventListener("DOMContentLoaded", (e)=>{
   hamburgerMenu(".panel-btn", ".panel",".menu a");
   digitalClock("#reloj","#activar-reloj","#desactivar-reloj")
   alarm("assets/alarma.mp3", "#activar-alarma", "#detener-alarma");
+  countdown("countdown","Jul 11, 2022 03:23:19", "Feliz cumpleaños")
 })
 
-d.addEventListener("keyup", (e) =>{
+d.addEventListener("keydown", (e) =>{
   shorcuts(e)
+  moveBall(e , ".ball",  ".stage")
 })
