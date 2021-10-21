078763daf48ca0b505cf09a4d5c8293a013979cc
nicohervith <niko_hervith@hotmail.com>
2021-10-21 09:32:48 -0300

    Responsive design

--- js/index_dom.js
+++ js/index_dom.js
@@ -5,6 +5,7 @@ import { moveBall } from "./dom/teclado.js";
 import countdown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/btn_scroll.js";
 import darkTheme from "./dom/dark_theme.js";
+import responsiveMedia from "./dom/obj_responsive.js";
 
 
 const d=document;
@@ -16,6 +17,19 @@ d.addEventListener("DOMContentLoaded", (e)=>{
   countdown("countdown","Jul 11, 2022 03:23:19", "Feliz cumpleaños")
   scrollTopButton(".scroll-top-btn");
   
+  responsiveMedia(
+    "youtube",
+    "(min-width: 1024px)",
+    `<a href= "https://www.youtube.com/watch?v=huie2_3Pekg" target="_blank" rel="noopener"> Ver video </a>`,
+    `<iframe width="560" height="315" src="https://www.youtube.com/embed/huie2_3Pekg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+  );
+
+   responsiveMedia(
+     "gmaps",
+     "(min-width: 1024px)",
+     `<a href= "https://goo.gl/maps/LppW8oHazPjUidZw9" target="_blank" rel="noopener"> Ver mapa</a>`,
+     `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.347359298184!2d-57.95844954961221!3d-34.92282438186194!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e62dd769b9d7%3A0x8e5a62b28764b5ef!2sCatedral%20de%20La%20Plata!5e0!3m2!1ses-419!2sar!4v1634819061890!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
+   );
 
 })
 
